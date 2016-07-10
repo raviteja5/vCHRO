afe68c54e3ddf2e3c02e9024fd385ea17cae2d7f
Mamun <srashed3@gatech.edu>
2016-07-10 18:22:23 -0400

    complete with feedback model

--- vCHRO-JAVA/src/vchro/SupervisorFeedback.java
+++ vCHRO-JAVA/src/vchro/SupervisorFeedback.java
@@ -5,12 +5,14 @@
  */
 package vchro;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import jess.JessException;
 
 /**
  *
@@ -346,50 +348,55 @@ public class SupervisorFeedback extends javax.swing.JFrame {
         idealEmp.setCoach(feedbackInput.get("coach"));
         
         String nowIdeal = idealEmp.toString();
-        //JOptionPane.showMessageDialog(this,"Previous: "+prevIdeal+"\nUpdate: "+nowIdeal);
-        ((MainGUI)parent).feedbackComplete(nowIdeal);
+        try {
+            //JOptionPane.showMessageDialog(this,"Previous: "+prevIdeal+"\nUpdate: "+nowIdeal);
+            ((MainGUI)parent).feedbackComplete(nowIdeal);
+        } catch (IOException | JessException ex) {
+            Logger.getLogger(SupervisorFeedback.class.getName()).log(Level.SEVERE, null, ex);
+            this.dispose();
+        }
         Logger.getLogger(SupervisorFeedback.class.getName()).log(Level.INFO,"Previous: "+prevIdeal+"\nUpdate: "+nowIdeal);
         
     }//GEN-LAST:event_btnSubmitMouseClicked
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SupervisorFeedback.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SupervisorFeedback.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SupervisorFeedback.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SupervisorFeedback.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    new SupervisorFeedback("test",null).setVisible(true);
-                } catch (Exception ex) {
-                    Logger.getLogger(SupervisorFeedback.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        });
-    }
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String args[]) {
+//        /* Set the Nimbus look and feel */
+//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+//         */
+//        try {
+//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+//                if ("Nimbus".equals(info.getName())) {
+//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+//                    break;
+//                }
+//            }
+//        } catch (ClassNotFoundException ex) {
+//            java.util.logging.Logger.getLogger(SupervisorFeedback.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (InstantiationException ex) {
+//            java.util.logging.Logger.getLogger(SupervisorFeedback.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (IllegalAccessException ex) {
+//            java.util.logging.Logger.getLogger(SupervisorFeedback.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+//            java.util.logging.Logger.getLogger(SupervisorFeedback.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        }
+//        //</editor-fold>
+//
+//        /* Create and display the form */
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                try {
+//                    new SupervisorFeedback("test",null).setVisible(true);
+//                } catch (Exception ex) {
+//                    Logger.getLogger(SupervisorFeedback.class.getName()).log(Level.SEVERE, null, ex);
+//                }
+//            }
+//        });
+//    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCancel;
